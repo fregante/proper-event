6e0e117517869234d2979d1900d8b77e34bba579
Federico <me@fregante.com>
2024-08-01 18:34:09 +0700

    Add support for init

--- index.ts
+++ index.ts
@@ -1,6 +1,109 @@
 // Update and keep in sync with `GlobalEventHandlersEventMap`
 // https://github.com/microsoft/TypeScript/blob/41b993bebe01c3401269839ffda4ae9a422ebb6b/src/lib/dom.generated.d.ts#L8901
 
+type EventInits = {
+	abort: UIEventInit;
+	animationcancel: AnimationEventInit;
+	animationend: AnimationEventInit;
+	animationiteration: AnimationEventInit;
+	animationstart: AnimationEventInit;
+	auxclick: MouseEventInit;
+	beforeinput: InputEventInit;
+	beforetoggle: EventInit;
+	blur: FocusEventInit;
+	cancel: EventInit;
+	canplay: EventInit;
+	canplaythrough: EventInit;
+	change: EventInit;
+	click: MouseEventInit;
+	close: EventInit;
+	compositionend: CompositionEventInit;
+	compositionstart: CompositionEventInit;
+	compositionupdate: CompositionEventInit;
+	contextmenu: MouseEventInit;
+	copy: ClipboardEventInit;
+	cuechange: EventInit;
+	cut: ClipboardEventInit;
+	dblclick: MouseEventInit;
+	drag: DragEventInit;
+	dragend: DragEventInit;
+	dragenter: DragEventInit;
+	dragleave: DragEventInit;
+	dragover: DragEventInit;
+	dragstart: DragEventInit;
+	drop: DragEventInit;
+	durationchange: EventInit;
+	emptied: EventInit;
+	ended: EventInit;
+	error: ErrorEventInit;
+	focus: FocusEventInit;
+	focusin: FocusEventInit;
+	focusout: FocusEventInit;
+	gotpointercapture: PointerEventInit;
+	input: EventInit;
+	invalid: EventInit;
+	keydown: KeyboardEventInit;
+	keypress: KeyboardEventInit;
+	keyup: KeyboardEventInit;
+	load: EventInit;
+	loadeddata: EventInit;
+	loadedmetadata: EventInit;
+	loadstart: EventInit;
+	lostpointercapture: PointerEventInit;
+	mousedown: MouseEventInit;
+	mouseenter: MouseEventInit;
+	mouseleave: MouseEventInit;
+	mousemove: MouseEventInit;
+	mouseout: MouseEventInit;
+	mouseover: MouseEventInit;
+	mouseup: MouseEventInit;
+	paste: ClipboardEventInit;
+	pause: EventInit;
+	play: EventInit;
+	playing: EventInit;
+	pointercancel: PointerEventInit;
+	pointerdown: PointerEventInit;
+	pointerenter: PointerEventInit;
+	pointerleave: PointerEventInit;
+	pointermove: PointerEventInit;
+	pointerout: PointerEventInit;
+	pointerover: PointerEventInit;
+	pointerup: PointerEventInit;
+	progress: ProgressEventInit;
+	ratechange: EventInit;
+	reset: EventInit;
+	resize: UIEventInit;
+	scroll: EventInit;
+	scrollend: EventInit;
+	securitypolicyviolation: SecurityPolicyViolationEventInit;
+	seeked: EventInit;
+	seeking: EventInit;
+	select: EventInit;
+	selectionchange: EventInit;
+	selectstart: EventInit;
+	slotchange: EventInit;
+	stalled: EventInit;
+	submit: SubmitEventInit;
+	suspend: EventInit;
+	timeupdate: EventInit;
+	toggle: EventInit;
+	touchcancel: TouchEventInit;
+	touchend: TouchEventInit;
+	touchmove: TouchEventInit;
+	touchstart: TouchEventInit;
+	transitioncancel: TransitionEventInit;
+	transitionend: TransitionEventInit;
+	transitionrun: TransitionEventInit;
+	transitionstart: TransitionEventInit;
+	volumechange: EventInit;
+	waiting: EventInit;
+	webkitanimationend: EventInit;
+	webkitanimationiteration: EventInit;
+	webkitanimationstart: EventInit;
+	webkittransitionend: EventInit;
+	wheel: WheelEventInit;
+};
+
 const events = {
 	abort: globalThis.UIEvent,
 	animationcancel: globalThis.AnimationEvent,
@@ -39,7 +142,6 @@ const events = {
 	focus: globalThis.FocusEvent,
 	focusin: globalThis.FocusEvent,
 	focusout: globalThis.FocusEvent,
-	formdata: globalThis.FormDataEvent,
 	gotpointercapture: globalThis.PointerEvent,
 	input: globalThis.Event,
 	invalid: globalThis.Event,
@@ -104,17 +206,27 @@ const events = {
 	webkittransitionend: globalThis.Event,
 	wheel: globalThis.WheelEvent,
 } as const;
+
 type Events = typeof events;
 
-function properEvent<Name extends keyof Events>(name: Name, options?: EventInit): Events[Name]['prototype'];
-function properEvent<Name extends string>(name: Name, options?: EventInit): CustomEvent;
-function properEvent<Name extends keyof Events>(name: Name, options?: EventInit): Events[Name]['prototype'] | CustomEvent {
-	return events[name] ? new events[name](name, options) : new CustomEvent(name, options);
+function properEvent<Name extends keyof Events>(name: Name, options?: EventInits[Name]): InstanceType<Events[Name]>;
+function properEvent<Name extends string>(name: Name, options?: CustomEventInit): CustomEvent;
+function properEvent<Name extends string>(name: Name, options?: EventInit): Event {
+	if (name in events) {
+		return new events[name as keyof Events](name, options);
+	}
+
+	return new CustomEvent(name, options);
 }
 
-const click: MouseEvent = properEvent('click');
+const click: MouseEvent = properEvent('click', {
+	which: 1,
+});
 document.dispatchEvent(click);
-const any: CustomEvent = properEvent('any');
+const any: CustomEvent = properEvent('any', {
+	bubbles: true,
+	detail: 1,
+});
 document.dispatchEvent(any);
 
 export default properEvent;
